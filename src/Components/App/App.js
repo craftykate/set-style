dea230cf1f09571ecb226b38d0b9332e3d39e08d
Kate McFaul <katemcfaul@gmail.com>
2019-01-20 12:04:10 -0800

    change styles

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    clicked: false
+  }
+
+  getStyle = () => {
+    if (this.state.clicked) {
+      return 'box clicked'
+    } else {
+      return 'box unclicked'
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    })
+  }
+
   render() {
     return (
-      <p>Hello World</p>
+      <div id="box"
+        className={this.getStyle()}
+        onClick={this.toggle}>
+        <p>ClassName: {this.getStyle()}</p>
+      </div>
     );
   }
 }
